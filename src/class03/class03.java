a78a80e97093b85ea0ff5de3c61cee2cb15d4524
lkamist00 <lkamist00@gmail.com>
2022-09-27 19:50:06 -0400

    second commit

--- src/class03/class03.java
+++ src/class03/class03.java
@@ -4,9 +4,11 @@ public class class03 {
 		
 	public static void main(String[] args) {
 	
-	System.out.println("This is a git class");		
+		System.out.println("This is a git class");		
+		System.out.println("This is just a change in my project");	
 			
 			
-			
+		
+		
 		}
 }
